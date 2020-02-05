114da1f1ff5adb551b56345757ec788faa4178f1
Salvatore <>
2020-02-05 09:18:49 +0000

    [Salvatore] [source:lang/ petitions] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ petitions] Mise a jour du bilan depuis https://trad.spip.net

--- lang/petitions_fr.php
+++ lang/petitions_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/petitions/lang
+// Fichier source, a modifier dans https://git.spip.net/spip/petitions.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
