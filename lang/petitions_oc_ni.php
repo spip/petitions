4609dff5fb9a3dafcc1c8f879dc61d5822972b27
Salvatore <>
2020-04-27 23:27:01 +0000

    [Salvatore] [source:lang/ petitions] Export depuis https://trad.spip.net de la langue oc_ni [Salvatore] [source:lang/ petitions] Mise a jour du bilan depuis https://trad.spip.net

--- lang/petitions_oc_ni.php
+++ lang/petitions_oc_ni.php
@@ -9,18 +9,28 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'aucune_signature' => 'Minga firma',
+
 	// B
+	'bouton_checkbox_envoi_message' => 'possibilitat de mandar un messatge',
 	'bouton_radio_activer_petition' => 'Activar la peticion',
 	'bouton_radio_pas_petition' => 'Minga de peticion',
 	'bouton_radio_petition_activee' => 'Peticion activada',
 	'bouton_radio_supprimer_petition' => 'Suprimir la peticion',
 
+	// C
+	'cfg_activer_petitions_label' => 'Volètz activar li peticions?',
+	'cfg_activer_petitions_label_non' => 'Desactivar li peticions',
+	'cfg_activer_petitions_label_oui' => 'Activar li peticions',
+
 	// F
-	'form_pet_adresse_site' => 'Adreça dau voastre sit', # MODIF
-	'form_pet_aucune_signature' => 'Minga de signatura correspoande à-n-aqueu còde...', # MODIF
+	'filtre' => 'Filtre:',
+	'form_pet_adresse_site' => 'Adreça URL dau voastre sit',
+	'form_pet_aucune_signature' => 'Minga signatura correspoande à-n-aqueu còde...',
 	'form_pet_confirmation' => 'Vorgatz confirmar la voastra signatura :',
 	'form_pet_deja_signe' => 'Avètz ja signat aqueu tèxt.',
-	'form_pet_envoi_mail_confirmation' => 'Aüra recebretz un corrier electronic de confirmacion sus @email@. Per validar la voastra signatura, cau visitar l’adreça web mencionada per aquest corrier.', # MODIF
+	'form_pet_envoi_mail_confirmation' => 'Aüra recebrètz un corrier electronic de confirmacion sus @email@. Per validar la voastra signatura, cau visitar l’adreça web mencionada per aquest corrier.',
 	'form_pet_mail_confirmation' => 'Boanjorn,
 
 Avètz demandat de signar la peticion:
@@ -41,22 +51,44 @@ serà refudada) :
 
 Mercé de la voastra participacion
 ',
-	'form_pet_nom_site2' => 'Nom dau voastre sit web', # MODIF
+	'form_pet_message_commentaire' => 'Un messatge, un comentari?',
+	'form_pet_nom_site2' => 'Nom dau voastre sit web',
 	'form_pet_probleme_liens' => 'Vorgatz tornar escriure lo voastre messatge en supprimant lu ligams ipertèxt.',
 	'form_pet_probleme_technique' => 'Problèma tecnic, li signaturas son temporàriament arrestadi.',
 	'form_pet_signature_validee' => 'La voastra signatura es validada. Mercé !',
 	'form_pet_site_deja_enregistre' => 'Aqueu sit es ja registrat',
 	'form_pet_url_invalide' => 'L’URL qu’avètz indicada es pas valida.',
 	'form_pet_votre_email' => 'La voastra adreça e-mail',
-	'form_pet_votre_nom' => 'Lo voastre nom ò pseudonim', # MODIF
+	'form_pet_votre_nom' => 'Lo voastre nom',
 	'form_pet_votre_site' => 'Se tenètz un sit, lo podètz indicar çai sota',
 
 	// I
+	'icone_suivi_petitions' => 'Sègre/gerar lu peticions',
 	'info_adresse_email' => 'ADREÇA E-MAIL:',
+	'info_fonctionnement_petition' => 'Foncionament de la peticion:',
 	'info_signature_supprimee' => 'Signatura suprimida',
-	'info_signature_supprimee_erreur' => 'Error : aquèu còde de supression correspoande à minga signatura', # MODIF
+	'info_signature_supprimee_erreur' => 'Error: aquest còde de supression correspoande à minga signatura',
 	'info_site_web' => 'SIT WEB :',
 
 	// L
-	'lien_reponse_article' => 'Respoasta à l’article'
+	'lien_reponse_article' => 'Respoasta à l’article',
+
+	// N
+	'nombre_signatures' => '@nb@ signatures',
+
+	// S
+	'signatures_aucune' => 'Minga',
+	'signatures_poubelle' => 'Suprimidi',
+	'signatures_prop' => 'En espera',
+	'signatures_publie' => 'Confirmadas',
+	'signatures_toutes' => 'Toti',
+	'statut_poubelle' => 'Suprimida',
+	'statut_prop' => 'En espera de validacion',
+	'statut_publie' => 'Confirmada',
+
+	// T
+	'titre_petition' => 'Peticion',
+	'titre_petitions' => 'Peticions',
+	'titre_selection_action' => 'Seleccion',
+	'titre_suivi_petition' => 'Seguit dei peticions'
 );
